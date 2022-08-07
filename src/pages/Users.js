6b0da52f7abb17578675e8ea35d493af1a85c7cf
Dev <dev@Majos-MacBook-Air.local>
2022-08-07 18:31:08 +0200

    Add handle error

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,38 +1,44 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 // Example normal data fetching in React
 export default function Users() {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    console.log("useEffect");
-
-    fetch("http://localhost:4000/superheroes")
-      .then((response) => response.json())
+    fetch('http://localhost:4000/superheroes')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Something went wrong');
+      })
       .then((data) => {
-        console.log("data", data);
         setUsers(data);
         setIsLoading(false);
-      });
+      })
+      .catch((error) => setError(error.message));
   }, []);
 
   if (isLoading) {
     return <h2>Loading...</h2>;
+  }
+  if (error) {
+    return <h2>{error}</h2>;
   }
 
   return (
     <>
       <h1>User list:</h1>
       <ul>
-        {users &&
-          users.map((user) => (
-            <li key={user.id}>
-              <a href={user.website} target="_blank" rel="noreferrer">
-                {user.name}
-              </a>
-            </li>
-          ))}
+        {users?.map((user) => (
+          <li key={user.id}>
+            <a href={user.website} target="_blank" rel="noreferrer">
+              {user.name}
+            </a>
+          </li>
+        ))}
       </ul>
     </>
   );
